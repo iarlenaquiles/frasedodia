bcf7d330a0830b0ae91fa9985cc51d238df6c7ac
Iarlen Aquiles <iarlensilva775@gmail.com>
2021-10-08 16:26:38 -0300

    initial commit

---
+++ app/src/main/java/com/aquiles/frasesdodia/MainActivity.java
@@ -0,0 +1,34 @@
+package com.aquiles.frasesdodia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void gerarNovaFrase(View view) {
+        TextView text = findViewById(R.id.textFrase);
+
+        String[] frases = {
+                "Frase 1",
+                "Frase 2",
+                "Frase 3",
+                "Frase 4"
+        };
+        int index = new Random().nextInt(4);
+
+        text.setText(frases[index]);
+    }
+
+}
